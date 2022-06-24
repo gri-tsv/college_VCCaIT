0518d3b18bfaec7bdf9db6cc7a5b192b40deb5e4
afa_gri.tsv <94740067+gri-tsv@users.noreply.github.com>
2022-06-24 14:57:34 +0300

    Add files via upload
    28.05 Calculation Library

---
+++ CalculationLibrary/ProgramCalcLib/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Calculations;
+namespace CalculationLib
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculationsLib a = new CalculationsLib();
+            TimeSpan[] startTimes = { new TimeSpan(10, 0, 0), new TimeSpan(11, 0, 0), new TimeSpan(16,0,0) };
+            int[]  time_ = { 30, 20 };
+            TimeSpan beginWorkingTime = new TimeSpan(8, 0, 0);
+            TimeSpan endWorkingTime = new TimeSpan(18, 0,0);
+            TimeSpan common = new TimeSpan(0, 30, 0);
+            int get_isklucheniya = 0;
+            double hours = (endWorkingTime - beginWorkingTime).TotalHours;
+            while (beginWorkingTime <= (endWorkingTime-common))
+            {
+                
+                if (beginWorkingTime == startTimes[get_isklucheniya])
+                {
+                    Console.WriteLine("yours");
+                    beginWorkingTime += TimeSpan.FromMinutes(time_[get_isklucheniya]);
+                    Console.WriteLine(beginWorkingTime + "-" + (beginWorkingTime + common));
+                    beginWorkingTime += common;
+                    get_isklucheniya += 1;
+                }
+                else
+                {
+                    Console.WriteLine(beginWorkingTime + "-" + (beginWorkingTime + common));
+                    beginWorkingTime += common;
+                }
+                
+            }
+                
+        }
+    }
+}
